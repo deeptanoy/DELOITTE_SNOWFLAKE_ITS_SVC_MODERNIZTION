03457e3adbfc70a7ec0206c97a7c91d96f0c0ab7
deeptanoy <109666756+deeptanoy@users.noreply.github.com>
2022-08-08 19:23:23 +0530

    Create R__create_proc.sql

---
+++ Scripts/Stored Proc/R__create_proc.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE CI_CD_DEMO.CICD_POC()
+RETURNS string
+LANGUAGE JAVASCRIPT
+AS
+$$
+    var sql1 = "create table ci_cd_demo.CICD_dummy1 (name string,date datetime)";
+    var statement1 = snowflake.createStatement({sqlText: sql1});
+    var result1 = statement1.execute();
+ 
+$$ 
+;
