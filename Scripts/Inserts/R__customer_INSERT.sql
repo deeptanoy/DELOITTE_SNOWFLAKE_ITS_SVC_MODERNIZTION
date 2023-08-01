d4a541029bec5bbc1730ed16586a049a423095b8
deeptanoy <109666756+deeptanoy@users.noreply.github.com>
2023-08-01 19:55:45 +0530

    Update R__customer_INSERT.sql

--- Scripts/Inserts/R__customer_INSERT.sql
+++ Scripts/Inserts/R__customer_INSERT.sql
@@ -1,4 +1,4 @@
 INSERT INTO "SNOWFLAKE_CI_CD_POC"."CI_CD_DEMO"."CUSTOMERS"
 (customer_id, last_name, first_name, favorite_website)
 VALUES
-(4000, 'Jackson', 'Joe', 'techonthenet.com');
+(400, 'Jackson', 'Joe', 'techonthenet.com');
